cc53184826cb551f174d48e2d8624ccf92c6f263
pleasego <372005816@qq.com>
2020-09-09 19:50:20 +0800

    xiong:第一次spring-mvc上传

--- src/main/java/com/xtkj/utils/Notice.java
+++ src/main/java/com/xtkj/utils/Notice.java
@@ -1,17 +1,36 @@
 package com.xtkj.utils;
 
+import lombok.extern.log4j.Log4j;
 import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
 @Component
 @Aspect
-@Slf4j
-public class Notice {
+@Log4j
+//@Slf4j
+class Notice {
+
     @Before("execution(* com.xtkj.service.impl.*.*(..))")
     public void before() {
-        log.debug("--------log4j日志--------------");
+        System.out.println("--------------><---------------");
+        log.info(".................please away.............................");
+        log.info("---------->前置通知<--------------");
+    }
+
+    @After("execution(* com.xtkj.service.impl.*.*(..))")
+    public void after() {
+        System.out.println("--------------><---------------");
+        log.info(".................please like.............................");
+        log.info("----------->后置通知<--------------");
+    }
+
+    @AfterThrowing(pointcut = "execution(* com.xtkj.service.impl.*.*(..))",throwing = "e")
+    public void throwing(Exception e){
+        log.debug(e.getMessage(),e);
     }
 
 
